349c419920a0fcef159ea0ca9725a109776a12e0
vg2425 <vg2425@columbia.edu>
2021-03-28 17:13:04 -0400

    added line grpah

--- src/containers/Handshake.js
+++ src/containers/Handshake.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import LineChart from '../components/LineChart';
 import styled from 'styled-components';
 
 const Handshake = () => {
     return (
-        <div>Handshake</div>
+        <div>Handshake
+            <LineChart width={400} height={300} />
+        </div>
     );
 };
-
+    
 
 export default Handshake;
