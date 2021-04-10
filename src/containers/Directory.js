1bb708af0f57c4721a180ae79c6c812e809b1dee
PatP15 <30728443+PatP15@users.noreply.github.com>
2021-04-09 20:11:26 -0400

    added to .css file

--- src/containers/Directory.js
+++ src/containers/Directory.js
@@ -1,16 +1,59 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, PureComponent } from 'react';
+import '../index.css';
 import BarGraph from '../components/BarGraph';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import DepartmentsBubble from '../components/BubbleChart';
-
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const datas = [
     [[1,2,3,4],[10, 30, 40, 20]],
     [[1,2,3,4,5,6],[10, 40, 30, 20, 50, 10]],
     [[1,2,3,4,5],[60, 30, 40, 20, 30]]
 ]
-
+const dataLine = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 //test to fill in 
 const dropdownStuff = ["Harambe", "Shrek", "Bob", "Dababy"];
 var i = 0;
@@ -40,7 +83,7 @@ const Directory = () => {
 
         <h1>Directory Scraper</h1>
         <h2>Undergrad Distribution Among Departments</h2>
-        <div className = "Bubble" style={{position: 'relative', width: 800, height: 800,}}>
+        <div className = "Bubble" style={{position: 'relative', width: 800, height: 800,}} class = "center">
             <DepartmentsBubble> </DepartmentsBubble>
         </div>
         <div className="BarGraph" style={{position: 'relative', width: 800, height: 500,}}>
@@ -65,6 +108,16 @@ const Directory = () => {
         
         </div>
         <h2>Historical Data</h2>
+        <h3>Attendance in each school</h3>
+        <div>
+        <LineChart width={500} height={300} data={dataLine}>
+            <XAxis dataKey="name"/>
+            <YAxis/>
+            <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+        </LineChart>
+        </div>
         </div>
 
     );
