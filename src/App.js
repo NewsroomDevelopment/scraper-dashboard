33c81cb03a173c3df3b0f72283dd082a6bc3c6a5
Joonsoo Lee <joonsoolee@dyn-160-39-151-211.dyn.columbia.edu>
2021-11-10 22:03:03 -0500

    new app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// App.js
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './containers/Home';
+import Directory from './containers/Directory';
+import  Vergil from './containers/Vergil';
+import Handshake from './containers/Handshake';
+import Confessions from './containers/Confessions';
+import Goco from './containers/Goco';
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/directory' component={Directory} />
+        <Route exact path='/handshake' component={Handshake} />
+        <Route exact path='/vergil' component={Vergil} />
+        <Route exact path='/confessions' component={Confessions} />
+        <Route exact path='/gocolumbialions' component={Goco} />
+      </Switch>
+    </Router>
+  );
+}
+export default App;
